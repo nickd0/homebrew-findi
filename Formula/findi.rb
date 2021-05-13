ef4e4b12d36fd63d47acac75d1a27b186dfffa14
Nick Donald <nick@farmshelf.com>
2021-05-13 18:49:34 -0400

    fix broken link

--- Formula/findi.rb
+++ Formula/findi.rb
@@ -1,7 +1,7 @@
 class Findi < Formula
   desc "Local network discovery tool"
   homepage "https://github.com/nickd0/findi"
-  url "https://github.com/nickd0/findi/releases/download/untagged-067849a6959db897dc2a/x86_64-apple-darwin.tar.gz"
+  url "https://github.com/nickd0/findi/releases/download/v0.2.0/x86_64-apple-darwin.tar.gz"
   sha256 "2addca44bbb7d2933f6ea9b1b4a5746e5a34a3573a1c9ebb4abe795179cd3fd1"
   version "0.2.0"
 
