5f5fba22f436a8f5f722648a73a5347707694324
Nick Donald <nick@farmshelf.com>
2021-05-13 18:47:12 -0400

    add findi formula

---
+++ Formula/findi.rb
@@ -0,0 +1,11 @@
+class Findi < Formula
+  desc "Local network discovery tool"
+  homepage "https://github.com/nickd0/findi"
+  url "https://github.com/nickd0/findi/releases/download/untagged-067849a6959db897dc2a/x86_64-apple-darwin.tar.gz"
+  sha256 "2addca44bbb7d2933f6ea9b1b4a5746e5a34a3573a1c9ebb4abe795179cd3fd1"
+  version "0.2.0"
+
+  def install
+    bin.install "findi"
+  end
+end
